792715608f7fc4601a42dfb69dd3b38a9c451f25
AbhiramRajeevan <abhiramrajiv14@gmail.com>
2022-07-06 22:40:07 +0530

    intital commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,46 @@
-import React, { useState } from 'react'
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
-import './Navbar.css'
+import { SidebarData } from './SidebarData';
+import './Navbar.css';
+import { IconContext } from 'react-icons';
 
-export const Navbar = () => {
-    const [sidebar, setSidebar] = useState(false)
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
 
-    const showSidebar = () => {
-        setSidebar(!sidebar)
-    }
-    return (
-        <>
-            <div className='navbar'>
-                <Link to="/" className="menu-bars">
-                    <FaIcons.FaBars onClick={showSidebar} />
-                </Link>
-            </div>
-            <nav className={sidebar ? "nav-menu active" : 'nav-menu'}>
-                <ul className="nav-menu-items">
-                    <li className="navbar-toggle">
-                        <Link to="#" className='menu-bars'>
-                            <AiIcons.AiOutlineClose />
-                        </Link>
-                    </li>
-                    <li>
-                        Projects
-                    </li>
-                    <li>
-                        About
-                    </li>
-                </ul>
-            </nav>
-        </>
-    )
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
+
+export default Navbar;
